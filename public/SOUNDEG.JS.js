4394a6a6c979b4bf3eddbedf04fa1d4f42a0a857
gilbertjacke <gilbertjacke@gmail.com>
2016-09-05 21:50:11 +0100

    added sounds

---
+++ public/SOUNDEG.JS.js
@@ -0,0 +1,56 @@
+/**
+ * Created by jackgilbert on 05/09/2016.
+ */
+// The midi notes of a scale
+var notes = [ 60, 62, 64, 65, 67, 69, 71];
+
+// For automatically playing the song
+var index = 0;
+var song = [
+  { note: 4, duration: 400, display: "D" },
+  { note: 0, duration: 200, display: "G" },
+  { note: 1, duration: 200, display: "A" },
+  { note: 2, duration: 200, display: "B" },
+  { note: 3, duration: 200, display: "C" },
+  { note: 4, duration: 400, display: "D" },
+  { note: 0, duration: 400, display: "G" },
+  { note: 0, duration: 400, display: "G" }
+];
+var trigger = 0;
+var autoplay = false;
+var osc;
+
+function setup() {
+  createCanvas(720, 400);
+  var div = createDiv("Click to play notes or ")
+  div.id("instructions");
+  var button = createA("#","play song automatically.");
+  button.parent("instructions");
+  // Trigger automatically playing
+  button.mousePressed(function() {
+    if (!autoplay) {
+      index = 0;
+      autoplay = true;
+    }
+  });
+
+  // A triangle oscillator
+  osc = new p5.TriOsc();
+  // Start silent
+  osc.start();
+  osc.amp(0);
+}
+
+// A function to play a note
+function playNote(note, duration) {
+  osc.freq(midiToFreq(note));
+  // Fade it in
+  osc.fade(0.5,0.2);
+
+  // If we sest a duration, fade it out
+  if (duration) {
+    setTimeout(function() {
+      osc.fade(0,0.2);
+    }, duration-50);
+  }
+}
--- public/sounds.js
+++ public/sounds.js
@@ -1,3 +1,103 @@
-/**
- * Created by jackgilbert on 05/09/2016.
- */
+// the midi notes of a scale
+var notes = [60, 62, 64, 65, 67, 69, 71, 74];
+
+var index = 0;
+var song = [
+  { note: 4, duration: 400, display: "D" },
+  { note: 0, duration: 200, display: "G" },
+  { note: 1, duration: 200, display: "A" },
+  { note: 2, duration: 200, display: "B" },
+  { note: 3, duration: 200, display: "C" },
+  { note: 4, duration: 400, display: "D" },
+  { note: 0, duration: 400, display: "G" },
+  { note: 0, duration: 400, display: "G" },
+];
+
+var trigger = 0;
+var autoplay = false;
+var osc;
+
+function setup() {
+  createCanvas(720, 400);
+  var button = createA("#", "button")
+    //trigger automatically playing
+    button.mousePressed(function(){
+      if(!autoplay) {
+        index = 0;
+        autoplay = true;
+      }
+    });
+  // a triangel oscillator
+  osc = new p5.TriOsc();
+  osc.start();
+  osc.amp(0);
+}
+
+// A function to play a note
+function playNote(note, duration) {
+  osc.freq(midiToFreq(note));
+  // Fade it in
+  osc.fade(0.5,0.2);
+
+  // If we sest a duration, fade it out
+  if (duration) {
+    setTimeout(function() {
+      osc.fade(0,0.2);
+    }, duration-50);
+  }
+}
+
+function draw () {
+  //if we are autoplaying and it's time of the next note
+  if (autoplay && millis() > trigger){
+    playNote(notes[song[index].note], song[index].duration);
+    trigger = millis() + song[index].duration;
+    //move to the next note
+    index ++;
+  } else if (index >= song.length){
+   autoplay = false;
+  }
+
+  //draw a keyboard
+
+  //the width for each key
+  var w = width / notes.length;
+  for (var i = 0; i < notes.length; i++) {
+    var x = i * w;
+    // If the mouse is over the key
+    if (mouseX > x && mouseX < x + w && mouseY < height) {
+      // If we're clicking
+      if (mouseIsPressed) {
+        fill(100,255,200);
+        // Or just rolling over
+      } else {
+        fill(127);
+      }
+    } else {
+      fill(200);
+    }
+
+    // Oh if we're playing teh song, let's highlight it too
+    if (autoplay && i === song[index-1].note) {
+      fill(100,255,200);
+    }
+
+    // Draw the key
+    rect(x, 0, w-1, height-1);
+  }
+
+}
+
+// When we click
+function mousePressed() {
+  // Map mouse to the key index
+  var key = floor(map(mouseX, 0, width, 0, notes.length));
+  playNote(notes[key]);
+}
+
+// Fade it out when we release
+function mouseReleased() {
+  osc.fade(0,0.5);
+}
+
+
